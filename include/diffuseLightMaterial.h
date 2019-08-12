2325d803e64d3397ac6fcf895aa667c70f06b37e
Joseph Triolo <triolo.j@husky.neu.edu>
2019-08-12 18:47:27 -0400

    dlm mem leak

--- include/diffuseLightMaterial.h
+++ include/diffuseLightMaterial.h
@@ -8,17 +8,24 @@
 #include "material.h"
 #include "texture.h"
 
-class diffuseLightMaterial: public material{
+class diffuseLightMaterial : public material {
 public:
-    diffuseLightMaterial(){}
-    diffuseLightMaterial(texture *a){
+    diffuseLightMaterial() {}
+
+    diffuseLightMaterial(texture *a) {
         emit = a;
     }
-    bool scatter(const ray& r_in, const hit_record& rec, vec3& attenuation, ray& scattered) const override {
+
+    ~diffuseLightMaterial() {
+        delete emit;
+    }
+
+    bool scatter(const ray &r_in, const hit_record &rec, vec3 &attenuation, ray &scattered) const override {
         return false;
     }
-    vec3 emitted(float u, float v, const vec3& p) const override{
-        return emit->value(u,v,p);
+
+    vec3 emitted(float u, float v, const vec3 &p) const override {
+        return emit->value(u, v, p);
     }
 
     texture *emit;
